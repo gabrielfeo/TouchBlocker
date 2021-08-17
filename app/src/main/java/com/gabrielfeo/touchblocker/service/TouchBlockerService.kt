09b2a497f5304f7ad7630191818599ba14c16df1
Gabriel Feo <gabriel@gabrielfeo.com>
2021-08-17 00:01:46 -0300

    Refactor overlay UI out of Service

--- app/src/main/java/com/gabrielfeo/touchblocker/service/TouchBlockerService.kt
+++ app/src/main/java/com/gabrielfeo/touchblocker/service/TouchBlockerService.kt
@@ -3,42 +3,24 @@ package com.gabrielfeo.touchblocker.service
 import android.annotation.SuppressLint
 import android.app.Service
 import android.content.Intent
-import android.graphics.PixelFormat
-import android.graphics.Typeface
-import android.os.Build
 import android.os.IBinder
-import android.util.DisplayMetrics
-import android.view.Gravity
-import android.view.View
 import android.view.WindowManager
-import android.widget.TextView
 import androidx.core.app.NotificationManagerCompat
-import androidx.core.content.ContextCompat
 import androidx.core.content.getSystemService
-import androidx.core.graphics.TypefaceCompat
 import com.gabrielfeo.touchblocker.R
+import com.gabrielfeo.touchblocker.ui.OverlayFactory
+import com.gabrielfeo.touchblocker.ui.OverlayFactoryImpl
 
 internal const val ACTION_TOGGLE_BLOCK = "com.gabrielfeo.touchblocker.ACTION_TOGGLE_BLOCK"
 internal const val EXTRA_TOGGLE_BLOCK_TARGET_VALUE = "ACTION_TOGGLE_BLOCK.value"
 internal const val REQUEST_TOGGLE_BLOCK = 1029
 
-@SuppressLint("SetTextI18n", "InflateParams")
 class TouchBlockerService : Service() {
 
     private val notificationFactory: NotificationFactory = NotificationFactoryImpl()
-    private val overlayView by lazy(LazyThreadSafetyMode.NONE) { createOverlayView() }
-
-    private fun createOverlayView(): View {
-        val textColor = ContextCompat.getColor(this, R.color.overlay_text_color)
-        val textTypeface = TypefaceCompat.create(this, null, Typeface.BOLD)
-        return TextView(this).apply {
-            gravity = Gravity.CENTER
-            rotation = -45f
-            text = "Blocking touches"
-            textSize = 28f
-            setTextColor(textColor)
-            typeface = textTypeface
-        }
+    private val overlayFactory: OverlayFactory = OverlayFactoryImpl()
+    private val overlay by lazy(LazyThreadSafetyMode.NONE) {
+        overlayFactory.createOverlay(this, checkNotNull(getSystemService()))
     }
 
     override fun onBind(intent: Intent?): IBinder? = null
@@ -51,7 +33,7 @@ class TouchBlockerService : Service() {
 
     private fun handleIntent(intent: Intent) {
         val shouldBlock = intent.getBooleanExtra(EXTRA_TOGGLE_BLOCK_TARGET_VALUE, true)
-        toggleOverlayView(active = shouldBlock)
+        toggleOverlay(active = shouldBlock)
         showNotification(currentlyBlocking = shouldBlock)
     }
 
@@ -65,44 +47,12 @@ class TouchBlockerService : Service() {
         notificationManager.notify(R.id.blocker_notification_id, notification)
     }
 
-    private fun toggleOverlayView(active: Boolean) {
+    private fun toggleOverlay(active: Boolean) {
         val windowManager = checkNotNull(getSystemService<WindowManager>())
         if (active) {
-            val screenSize = windowManager.getScreenSizeCompat()
-            val layoutParams = createOverlayLayoutParams(screenSize)
-            windowManager.addView(overlayView, layoutParams)
+            windowManager.addView(overlay.view, overlay.layoutParams)
         } else {
-            windowManager.removeView(overlayView)
-        }
-    }
-
-    private fun createOverlayLayoutParams(screenSize: Pair<Int, Int>): WindowManager.LayoutParams {
-        return WindowManager.LayoutParams(
-            WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
-            WindowManager.LayoutParams.FLAG_DIM_BEHIND,
-            PixelFormat.TRANSLUCENT,
-        ).apply {
-            val (screenWidth, screenHeight) = screenSize
-            width = screenWidth
-            height = screenHeight
-            gravity = Gravity.CENTER
-            dimAmount = 0.6f
-        }
-    }
-
-    @Suppress("Deprecation")
-    private fun WindowManager.getScreenSizeCompat(): Pair<Int, Int> {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            val windowMetrics = currentWindowMetrics
-            windowMetrics.bounds.let {
-                it.width() to it.height()
-            }
-        } else {
-            val displayMetrics = DisplayMetrics()
-            defaultDisplay.getMetrics(displayMetrics)
-            displayMetrics.let {
-                it.widthPixels to it.heightPixels
-            }
+            windowManager.removeView(overlay.view)
         }
     }
 
---
+++ app/src/main/java/com/gabrielfeo/touchblocker/ui/Overlay.kt
@@ -0,0 +1,78 @@
+package com.gabrielfeo.touchblocker.ui
+
+import android.content.Context
+import android.graphics.PixelFormat
+import android.graphics.Typeface
+import android.os.Build
+import android.util.DisplayMetrics
+import android.view.Gravity
+import android.view.View
+import android.view.WindowManager
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.TypefaceCompat
+import com.gabrielfeo.touchblocker.R
+
+data class Overlay(
+    val view: View,
+    val layoutParams: WindowManager.LayoutParams,
+)
+
+interface OverlayFactory {
+    fun createOverlay(context: Context, windowManager: WindowManager): Overlay
+}
+    
+class OverlayFactoryImpl : OverlayFactory {
+
+    override fun createOverlay(context: Context, windowManager: WindowManager): Overlay {
+        val screenSize = windowManager.getScreenSizeCompat()
+        return Overlay(
+            view = createView(context),
+            layoutParams = createOverlayLayoutParams(screenSize),
+        )
+    }
+
+    private fun createView(context: Context): TextView {
+        val textColor = ContextCompat.getColor(context, R.color.overlay_text_color)
+        val textTypeface = TypefaceCompat.create(context, null, Typeface.BOLD)
+        return TextView(context).apply {
+            gravity = Gravity.CENTER
+            rotation = -45f
+            text = context.getString(R.string.overlay_text)
+            textSize = 28f
+            setTextColor(textColor)
+            typeface = textTypeface
+        }
+    }
+
+    private fun createOverlayLayoutParams(screenSize: Pair<Int, Int>): WindowManager.LayoutParams {
+        return WindowManager.LayoutParams(
+            WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+            WindowManager.LayoutParams.FLAG_DIM_BEHIND,
+            PixelFormat.TRANSLUCENT,
+        ).apply {
+            val (screenWidth, screenHeight) = screenSize
+            width = screenWidth
+            height = screenHeight
+            gravity = Gravity.CENTER
+            dimAmount = 0.6f
+        }
+    }
+
+    @Suppress("Deprecation")
+    private fun WindowManager.getScreenSizeCompat(): Pair<Int, Int> {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val windowMetrics = currentWindowMetrics
+            windowMetrics.bounds.let {
+                it.width() to it.height()
+            }
+        } else {
+            val displayMetrics = DisplayMetrics()
+            defaultDisplay.getMetrics(displayMetrics)
+            displayMetrics.let {
+                it.widthPixels to it.heightPixels
+            }
+        }
+    }
+    
+}
