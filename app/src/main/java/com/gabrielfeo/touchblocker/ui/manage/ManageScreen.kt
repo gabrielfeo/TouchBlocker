c90f3978a725ca1f6a15c6daef0a7143b1400763
Gabriel Feo <gabriel@gabrielfeo.com>
2021-09-08 23:40:34 -0300

    Update blocking started message to new mechanism

--- app/src/main/java/com/gabrielfeo/touchblocker/ui/manage/ManageScreen.kt
+++ app/src/main/java/com/gabrielfeo/touchblocker/ui/manage/ManageScreen.kt
@@ -1,16 +1,23 @@
 package com.gabrielfeo.touchblocker.ui.manage
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun ManageScreen(
     canBlock: Boolean,
@@ -28,8 +35,9 @@ fun ManageScreen(
         ) {
             if (canBlock) {
                 if (serviceRunning) {
-                    RunningStatusText(modifier = Modifier.offset(y = (-200).dp))
-                } else {
+                    CurrentlyBlockingMessage(modifier = Modifier.offset(y = (-200).dp))
+                }
+                AnimatedVisibility(visible = !serviceRunning) {
                     StartBlockingButton(onStartBlockingClick)
                 }
             } else {
@@ -66,19 +74,30 @@ private fun StartBlockingButton(onStartBlockingClick: () -> Unit) {
 }
 
 @Composable
-private fun RunningStatusText(modifier: Modifier = Modifier) {
+private fun CurrentlyBlockingMessage(modifier: Modifier = Modifier) {
     Column(
         modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        StatusTextParagraph("It works!")
+        MessageTextParagraph("It works!")
+        Spacer(modifier = Modifier.size(4.dp))
+        MessageTextParagraph("Please use the notification to toggle blocking on/off")
         Spacer(modifier = Modifier.size(2.dp))
-        StatusTextParagraph("Please use the notification to toggle blocking on/off :)")
+        val tip = buildAnnotatedString {
+            pushStyle(SpanStyle(fontWeight = FontWeight.Bold))
+            append("Pro tip: ")
+            pop()
+            append("you can double-tap the screen twice to stop blocking")
+        }
+        MessageTextParagraph(tip)
     }
 }
 
 @Composable
-private fun StatusTextParagraph(text: String) {
+private fun MessageTextParagraph(text: String) = MessageTextParagraph(AnnotatedString(text))
+
+@Composable
+private fun MessageTextParagraph(text: AnnotatedString) {
     Text(text, color = Color.Black, textAlign = TextAlign.Center, fontSize = 18.sp)
 }
 
